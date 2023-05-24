7444ccad8573a2f13d370965335280ef36dd4f95
Samuele Tatini <44943668+samueletatini@users.noreply.github.com>
2023-05-24 10:28:09 +0200

    Update studenti.cpp

--- studenti.cpp
+++ studenti.cpp
@@ -1,7 +1,6 @@
 #include<iostream>
 #include<fstream>
 #include<string>
-
 using namespace std;
 
 struct studente{
@@ -9,62 +8,75 @@ struct studente{
     string cognome;
 };
 
-void scrivistudente(){
-    studente s;
-    int scelta;
-    cout<<"Inserisci 1 per nome, 2 per nome e cognome: ";
-    cin>>scelta;
+void scriviStudente(){
+    int numStudenti;
+    cout<<"Inserisci il numero di studenti da inserire:";
+    cin>>numStudenti;
     cin.ignore();
 
-    cout<<"Inserisci il nome dello studente: ";
-    getline(cin, s.nome);
+    fstream file("lista.txt",ios::out|ios::app);
 
-    fstream file("lista.txt", ios::out | ios::app);
-    if(scelta==2){
-        cout<<"Inserisci il cognome dello studente: ";
-        getline(cin, s.cognome);
-        file<<s.nome<<' '<<s.cognome<<'\n';
+    for(int i=0;i<numStudenti;i++){
+        studente s;
+        int scelta;
+        cout<<"Inserisci 1 per nome, 2 per nome e cognome per lo studente "<<i+1<<":";
+        cin>>scelta;
+        cin.ignore();
+
+        cout<<"Inserisci il nome dello studente:";
+        getline(cin,s.nome);
+
+        if(scelta==2){
+            cout<<"Inserisci il cognome dello studente:";
+            getline(cin,s.cognome);
+            file<<s.nome<<';'<<s.cognome<<'\n';
+        }
+        else{
+            file<<s.nome<<'\n';
+        }
     }
-    else{
-        file<<s.nome<<'\n';
-    }
+
     file.close();
 }
 
-
-void visualizzalista(){
+void visualizzaLista(){
     string linea;
     fstream file("lista.txt",ios::in);
     while(getline(file,linea)){
-        cout<<linea<<"\n";
+        string nome=linea;
+        string cognome="";
+        int delimitatore=linea.find(';');
+        if(delimitatore!=-1){
+            nome=linea.substr(0,delimitatore);
+            cognome=linea.substr(delimitatore+1);
+        }
+        cout<<"Nome:"<<nome<<", Cognome:"<<cognome<<'\n';
     }
     file.close();
 }
 
-void clear()
-{
+void clear(){
     ofstream file("lista.txt");
     file<<"";
 }
 
 int main(){
-	clear();
+    clear();
     int scelta;
     do{
-        cout<<"Inserisci 1 per scrivere uno studente sulla lista, 2 per visualizzare la lista, 0 per uscire: ";
+        cout<<"Inserisci 1 per scrivere uno studente sulla lista, 2 per visualizzare la lista, 0 per uscire:";
         cin>>scelta;
 
         if(scelta==1){
-            scrivistudente();
-        }else if(scelta==2){
-            visualizzalista();
-        }else if(scelta!=0){
+            scriviStudente();
+        }
+        else if(scelta==2){
+            visualizzaLista();
+        }
+        else if(scelta!=0){
             cout<<"Scelta non valida."<<endl;
         }
     }while(scelta!=0);
-    
+
     return 0;
 }
-
-
-
